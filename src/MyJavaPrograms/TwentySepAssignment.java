405199629cbf41386d5be1c01f61341fb7e33337
Admin <atifmohd635@gmail.com>
2025-09-21 12:31:43 +0530

    Selenium Learning First Code

---
+++ src/MyJavaPrograms/TwentySepAssignment.java
@@ -0,0 +1,11 @@
+package MyJavaPrograms;
+
+import java.io.PrintStream;
+
+public class TwentySepAssignment {
+    public static void main(String[] args) {
+        int num=2;
+        String result = (num % 2 == 0) ? "Even num" : "Odd number";
+        System.out.println(result);
+    }
+}
